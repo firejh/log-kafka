1dbb3a255a3fdf74d505419be6df2ae141931121
AlexStocks <alexstocks@foxmail.com>
2018-05-09 22:53:30 +0800

    delete getty

--- assembly/bin/load.sh
+++ assembly/bin/load.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # ******************************************************
-# DESC    : getty app devops script
+# DESC    : log-kafka devops script
 # AUTHOR  : Alex Stocks
 # VERSION : 1.0
 # LICENCE : LGPL V3
